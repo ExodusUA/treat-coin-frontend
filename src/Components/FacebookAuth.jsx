2a1e6e506eb1bf590cdc344df8012c346447a205
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2023-10-16 23:02:01 +0300

    Update FacebookAuth.jsx

--- src/Components/FacebookAuth.jsx
+++ src/Components/FacebookAuth.jsx
@@ -10,15 +10,17 @@ function FacebookAuth({ loginViaFacebook, setLoading }) {
     }
 
     return (
-        <LoginSocialFacebook scope='email' appId={process.env.REACT_APP_FACEBOOK_APP_ID} onResolve={(response) => {
-            getFacebookEmail(response)
-        }} 
-        onReject={(response) => {
+        <LoginSocialFacebook scope='email' fields='email' appId={process.env.REACT_APP_FACEBOOK_APP_ID} onResolve={(response) => {
             console.log(response)
+            getFacebookEmail(response)
         }}
+            onReject={(response) => {
+                console.log(response)
+            }}
             fieldsProfile={
                 'email'
             }>
+
             <button type="button" className='border-[1px] border-[#282B54] bg-white px-[16px] py-[16px] flex items-center gap-3 rounded-[18px] w-[155px] sm:w-[165px]'>
                 <img className='w-[30px]' src={facebook} alt="Facebook" />
                 <p className='text-[#2C3BCB] text-[16px] sm:text-[18px]'>Facebook</p>
